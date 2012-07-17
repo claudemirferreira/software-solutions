7eb1aa1393a26279ffe16db9a992b332c3007123
petersongfarias <petersongfarias@411d2c86-007b-7a0f-e338-9046a2b85cf2>
2012-07-17 12:30:35 +0000

    crud grupo

---
+++ trunk/portal/desenvolvimento/codigo/ss-authenticator/src/main/java/br/ss/authenticator/service/impl/GrupoService.java
@@ -0,0 +1,35 @@
+package br.ss.authenticator.service.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.ss.authenticator.model.dao.IGrupoDAO;
+import br.ss.authenticator.model.entity.Grupo;
+import br.ss.authenticator.service.BaseService;
+import br.ss.core.model.dao.IAbstractDAO;
+
+@Named
+@ConversationScoped
+public class GrupoService extends BaseService<Grupo> implements Serializable{
+
+	@Inject
+	private IGrupoDAO grupoDAO;
+	
+	@Override
+	protected IAbstractDAO<Grupo> getDao() {
+		return grupoDAO;
+	}
+	
+	@Produces @Named 
+	public List<Grupo> gruposAtivos() {
+		Grupo grupo = new Grupo();
+		grupo.setCsAtivo(true);	// somente ativos
+		return grupoDAO.searchByEntity(grupo);
+	}
+
+}
