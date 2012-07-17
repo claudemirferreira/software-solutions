14f3d2131093fe8658abf190e8820be4291e9a32
petersongfarias <petersongfarias@411d2c86-007b-7a0f-e338-9046a2b85cf2>
2012-07-17 12:30:35 +0000

    crud grupo

---
+++ portal/desenvolvimento/codigo/ss-authenticator/src/main/java/br/ss/authenticator/model/dao/IGrupoDAO.java
@@ -0,0 +1,11 @@
+package br.ss.authenticator.model.dao;
+
+import javax.ejb.Local;
+
+import br.ss.authenticator.model.entity.Grupo;
+import br.ss.core.model.dao.IAbstractDAO;
+
+@Local
+public interface IGrupoDAO extends IAbstractDAO<Grupo>{
+
+}
---
+++ portal/desenvolvimento/codigo/ss-authenticator/src/main/java/br/ss/authenticator/model/dao/impl/GrupoDAO.java
@@ -0,0 +1,50 @@
+package br.ss.authenticator.model.dao.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.enterprise.context.Dependent;
+import javax.persistence.Query;
+
+import br.ss.authenticator.model.dao.IGrupoDAO;
+import br.ss.authenticator.model.entity.Grupo;
+import br.ss.core.annotation.stereotype.DAO;
+import br.ss.core.model.dao.impl.AbstractDAO;
+
+@Dependent
+@Stateless
+@DAO
+public class GrupoDAO extends AbstractDAO<Grupo> implements IGrupoDAO{
+
+	@SuppressWarnings("unchecked")
+	public List<Grupo> searchByEntity( Grupo grupo ) {
+		
+		StringBuilder s = new StringBuilder();
+		List<String> condictions = new ArrayList<String>();
+		
+		s.append(" select s from Grupo s ");
+		if ( notEmpty(grupo.getTxGrupo()) ) {
+			condictions.add(" lower(s.txGrupo) like :txGrupo ");
+		}
+		
+		if ( notEmpty(grupo.getCsAtivo()) ) {
+			condictions.add(" s.csAtivo = :csAtivo ");
+		}
+		
+		String orderBy = " order by s.txGrupo ";
+		
+		Query q = this.entityManager.createQuery( generateHql(s.toString(), condictions) + orderBy );
+		
+		if ( notEmpty(grupo.getTxGrupo() ) ) {
+			q.setParameter("txGrupo", "%" + grupo.getTxGrupo().trim().toLowerCase() + "%" );
+		}
+		
+		if ( notEmpty(grupo.getCsAtivo() ) ) {
+			q.setParameter("csAtivo", ( Boolean ) grupo.getCsAtivo());
+		}
+		
+		return q.getResultList();
+	}
+
+}
