a2cbdd533b81fd7af2663c182151b0aae77d8b7c
alvaraesam@gmail.com <alvaraesam@gmail.com@411d2c86-007b-7a0f-e338-9046a2b85cf2>
2013-01-07 13:29:31 +0000

    add user

---
+++ trunk/centralaamar/desenvolvimento/fonte/centralaamar/src/main/java/br/com/ss/centralaamar/model/entity/User.java
@@ -0,0 +1,14 @@
+package br.com.ss.centralaamar.model.entity;
+
+public class User {
+	private String adress;
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+}
