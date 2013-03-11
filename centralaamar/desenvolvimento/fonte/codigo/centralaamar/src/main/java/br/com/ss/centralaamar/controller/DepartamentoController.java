7d36be96dbb43bc9e7e24e903d4fcabcb9354e88
alvaraesam <alvaraesam@gmail.com>
2013-03-11 17:59:35 +0000

    ajuste no banco

---
+++ centralaamar/desenvolvimento/fonte/codigo/centralaamar/src/main/java/br/com/ss/centralaamar/controller/DepartamentoController.java
@@ -0,0 +1,36 @@
+package br.com.ss.centralaamar.controller;
+
+import java.sql.SQLException;
+
+import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import br.com.ss.centralaamar.model.entity.Departamento;
+import br.com.ss.centralaamar.service.IDepartamentoService;
+import br.com.ss.centralaamar.service.IService;
+
+@Controller
+@Named
+@Scope("session")
+public class DepartamentoController extends GenericBean<Departamento> {
+
+	private static final long serialVersionUID = 6031730028318406591L;
+
+	@Autowired
+	private IDepartamentoService service;
+
+	@Override
+	protected IService<Departamento> getService() {
+		return service;
+	}
+
+	public String save() throws SQLException {
+		this.entity.setNome(this.entity.getNome().toUpperCase());
+		return super.save();
+
+	}
+
+}
