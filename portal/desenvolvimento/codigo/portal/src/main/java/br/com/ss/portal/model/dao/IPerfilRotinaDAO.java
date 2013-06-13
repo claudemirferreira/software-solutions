8b2cb9c1194217ffc9bfb696743a7eb5108eb006
alvaraesam <alvaraesam@gmail.com>
2013-06-13 00:46:39 +0000

    aa

--- portal/desenvolvimento/codigo/portal/src/main/java/br/com/ss/portal/model/dao/IPerfilRotinaDAO.java
+++ portal/desenvolvimento/codigo/portal/src/main/java/br/com/ss/portal/model/dao/IPerfilRotinaDAO.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import br.com.ss.portal.model.entity.Perfil;
 import br.com.ss.portal.model.entity.PerfilRotina;
+import br.com.ss.portal.model.entity.Sistema;
+import br.com.ss.portal.model.entity.Usuario;
 
 public interface IPerfilRotinaDAO extends IAbstractDAO<PerfilRotina> {
 	
 	public List<PerfilRotina> searchByEntity(Perfil entity);
+	
+	public List<Object[]> searchPerfilUsuario(Usuario usuario, Sistema sistema);
 
 }
