776188baa20dd4dc726a2124f574767b5495993b
alvaraesam@gmail.com <alvaraesam@gmail.com@411d2c86-007b-7a0f-e338-9046a2b85cf2>
2014-07-01 10:33:49 +0000

    ajuste na impressao

--- trunk/academico/desenvolvimento/fonte/shekhinah/src/main/java/br/com/ss/academico/controlador/CursoControlador.java
+++ trunk/academico/desenvolvimento/fonte/shekhinah/src/main/java/br/com/ss/academico/controlador/CursoControlador.java
@@ -1,30 +1,13 @@
 package br.com.ss.academico.controlador;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
 
 import br.com.ss.academico.dominio.Curso;
 import br.com.ss.academico.servico.CursoServico;
-import br.com.ss.core.seguranca.dominio.Usuario;
 import br.com.ss.core.seguranca.servico.IService;
 import br.com.ss.core.web.controlador.ControladorGenerico;
-import br.com.ss.core.web.utils.Util;
-
-import com.lowagie.text.DocumentException;
 
 @ManagedBean
 @SessionScoped
@@ -34,14 +17,11 @@ public class CursoControlador extends ControladorGenerico<Curso> {
 
 	@ManagedProperty(value = "#{cursoServicoImpl}")
 	private CursoServico servico;
-	
+
 	private String nomeRelatorio = "curso.jasper";
-	
-	private boolean visualizar = false;
-	
+
 	@Override
 	protected String getNomeRelatorio() {
-		// FIXME #Peninha ver relatorio
 		return this.nomeRelatorio;
 	}
 
@@ -58,21 +38,8 @@ public class CursoControlador extends ControladorGenerico<Curso> {
 		this.servico = servico;
 	}
 
-	public boolean isVisualizar() {
-		return visualizar;
-	}
-
-	public void setVisualizar(boolean visualizar) {
-		this.visualizar = visualizar;
-	}
-
 	public void setNomeRelatorio(String nomeRelatorio) {
 		this.nomeRelatorio = nomeRelatorio;
 	}
-	
-	public void visualiarRelatorio() {
-		this.visualizar = true;
-	}
 
-	
 }
