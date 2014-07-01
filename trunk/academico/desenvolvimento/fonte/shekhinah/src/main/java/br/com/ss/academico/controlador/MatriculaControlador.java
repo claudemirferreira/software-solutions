bf1d1e987a0d78fe439d56559e1d9331a1222360
alvaraesam <alvaraesam@411d2c86-007b-7a0f-e338-9046a2b85cf2>
2014-07-01 14:29:57 +0000

    ajuste na impressao do contrato

--- trunk/academico/desenvolvimento/fonte/shekhinah/src/main/java/br/com/ss/academico/controlador/MatriculaControlador.java
+++ trunk/academico/desenvolvimento/fonte/shekhinah/src/main/java/br/com/ss/academico/controlador/MatriculaControlador.java
@@ -1,6 +1,5 @@
 package br.com.ss.academico.controlador;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,35 +7,26 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.jasperreports.engine.JRDataSource;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporter;
 import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.engine.export.JRPdfExporterParameter;
-
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
-
 import br.com.ss.academico.dominio.Aluno;
 import br.com.ss.academico.dominio.Curso;
 import br.com.ss.academico.dominio.Matricula;
@@ -51,7 +41,6 @@ import br.com.ss.academico.servico.MatriculaServico;
 import br.com.ss.academico.servico.TurmaServico;
 import br.com.ss.core.seguranca.servico.IService;
 import br.com.ss.core.web.controlador.ControladorGenerico;
-import br.com.ss.core.web.enumerated.Constants;
 import br.com.ss.core.web.ireport.RelatorioUtil;
 import br.com.ss.core.web.utils.ReflectionsUtil;
 
@@ -72,14 +61,20 @@ public class MatriculaControlador extends ControladorGenerico<Matricula> {
 
 	@ManagedProperty(value = "#{cursoServicoImpl}")
 	private CursoServico servicoCurso;
-	
+
 	@ManagedProperty(value = "#{empresaServicoImpl}")
 	private EmpresaServico empresaServico;
-	
+
+	@ManagedProperty(value = "#{turmaServicoImpl}")
+	private TurmaServico turmaServico;
+
 	private List<SelectItem> statusList;
 
 	private List<SelectItem> turnoList;
-	
+
+	private static final String PATH_REPORT = "resources" + File.separator
+			+ "jasper" + File.separator;
+
 	@Override
 	protected void init() {
 		statusList = new ArrayList<SelectItem>();
@@ -105,14 +100,13 @@ public class MatriculaControlador extends ControladorGenerico<Matricula> {
 		return servico;
 	}
 
-	
 	@Override
 	public String novo() {
 		// inicializa o bean alunoMatriculaControlador
 		ReflectionsUtil.invokeBean("alunoMatriculaControlador", "novo");
 		return super.novo();
 	}
-	
+
 	/**
 	 * Lista os Alunos - para a lista do auto-complete da tela de pesquisa.
 	 */
@@ -129,157 +123,46 @@ public class MatriculaControlador extends ControladorGenerico<Matricula> {
 		return servicoCurso.findByNomeLike(nome);
 	}
 
-
 	public String salvar() {
-
 		// gerar boletim
 		if (this.entidade.getIdMatricula() == null) {
 			this.boletimServico.gerarBoletim(this.entidade);
 		}
-		
+
 		return super.salvar();
 
 	}
 
-	public void imprimir(Matricula matricula) {
-		List<Matricula> lista = new ArrayList<Matricula>();
-		lista.add(matricula);
-		Map<String, Object> parametros = new HashMap<String, Object>();
+	public void imprimirContrato(Matricula matricula)
+			throws FileNotFoundException {
 
-		relatorioUtil.gerarRelatorioComDownload(lista, parametros, "contrato-parte1.jasper");
-		
-		// FIXME #Peninha padronizar a impressao
-
-	}
-
-	public void imprimircontrato(Matricula matricula) throws FileNotFoundException, JRException {
-		// TODO criar o relatorio..
-		List<Matricula> listMat = new ArrayList<Matricula>();
-		listMat.add(matricula);
-		relatorioUtil.gerarRelatorioWeb(listMat, null, "contrato-parte1.jasper");
-		
-		// FIXME #Peninha padronizar a impressao
-		
-	}
-
-	// FIXME #Peninha, validar se está utilizando este metodo, se nao, remover
-	private void gerar(Matricula matricula) throws MalformedURLException {
-		Map<String, Object> parametros = new HashMap<String, Object>();
-		List<Matricula> lista = new ArrayList<Matricula>();
-		String nome = "contrato-parte1.jasper";
-
-		gerarRelatorio(null, parametros, lista, nome);
-
-	}
-
-	private StreamedContent gerarRelatorio(String localRelatorio, Map<String, Object> parametros, 
-								List<Matricula> lista2, String nomeArquivoSaida) throws MalformedURLException {
-
-		InputStream isRelatorio = null;
-		try {
-			ByteArrayOutputStream relat = new ByteArrayOutputStream();
-			ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-			ServletContext contextS = (ServletContext) externalContext.getContext();
-			// String arquivo = contextS.getRealPath("/relatorios/" +
-			// localRelatorio);
-			
-			String arquivo = "c:/relatorio/contrato-parte1.jasper";
-
-			JasperPrint print = JasperFillManager.fillReport(
-					new FileInputStream(new File(arquivo)), parametros,
-					new JRBeanCollectionDataSource(lista2));
-			JRExporter exporter = new JRPdfExporter();
-			exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
-			exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, relat);
-			exporter.exportReport();
-			isRelatorio = new ByteArrayInputStream(relat.toByteArray());
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			showMessage(Constants.MSG_ERRO, FacesMessage.SEVERITY_ERROR);
-			return null;
-		}
-
-		return new DefaultStreamedContent(isRelatorio, "application/pdf", nomeArquivoSaida + ".pdf");
-
-	}
-	
-	/* FIXME remover metodos de teste..
-	
-	public void teste(Matricula matricula) throws IOException, JRException {
-
-		Map parametros = new HashMap();
-		parametros.put("empresa", empresaServico.findOne(1l));
-
-		List<Matricula> lista = new ArrayList<Matricula>();
-		lista.add(matricula);
+		// matricula.setTurma(turmaServico.findByMatricula(matricula));
+		ExternalContext econtext = FacesContext.getCurrentInstance()
+				.getExternalContext();
 
 		FacesContext context = FacesContext.getCurrentInstance();
 
-		HttpServletResponse response = (HttpServletResponse) context
-				.getExternalContext().getResponse();
+		String webPath = context.getExternalContext().getRealPath("/");
+		String reportPath1 = webPath + PATH_REPORT + "contrato-parte1.jasper";
+		String reportPath2 = webPath + PATH_REPORT + "contrato-parte2.jasper";
 
-		ServletOutputStream responseStream = response.getOutputStream();
+		InputStream stream1 = new FileInputStream(reportPath1);
+		InputStream stream2 = new FileInputStream(reportPath2);
 
-		String caminho = "c:/relatorio/contrato-parte1.jasper";
+		List<JasperPrint> j = new ArrayList<JasperPrint>();
 
-		response.setContentType("application/pdf");
-		response.setHeader("Content-Disposition",
-				"attachment; filename=\"relatorio.pdf\"");
-
-		// JasperPrint preencher = JasperFillManager.fillReport(caminho,
-		// null,new JRBeanCollectionDataSource(lista));
-
-		JasperRunManager.runReportToPdfStream(new FileInputStream(new File(
-				caminho)), response.getOutputStream(), parametros,
-				new JRBeanCollectionDataSource(lista));
-
-		// JasperExportManager.exportReportToPdfStream(preencher,responseStream);
-
-		responseStream.flush();
-		responseStream.close();
-		context.renderResponse();
-		context.responseComplete();
-
-	}
-	*/
-	
-	 
-	@ManagedProperty(value = "#{turmaServicoImpl}")
-	private TurmaServico turmaServico;
-	
-	// FIXME #Peninha: refatorar esse metodo
-	
-	public void imprimirRelatorio(Matricula matricula) throws FileNotFoundException {
-		
-//		matricula.setTurma(	turmaServico.findByMatricula(matricula));
-
-		ExternalContext econtext = FacesContext.getCurrentInstance()
-				.getExternalContext();
-		InputStream stream1 = new FileInputStream( "C:\\relatorio\\contrato-parte1.jasper");
-		InputStream stream2 = new FileInputStream( "C:\\relatorio\\contrato-parte2.jasper");
-
-		List j = new ArrayList();
-		
 		List<Matricula> list = new ArrayList<Matricula>();
 		list.add(matricula);
-		
+
 		JRDataSource jrRS = new JRBeanCollectionDataSource(list);
 
 		try {
-			Map params = new HashMap(); // preenche os parâmetros do relatório 1
-			
+			Map<String, Object> params = new HashMap<String, Object>();
 			params.put("empresa", empresaServico.findOne(1l));
-			
-			// stream1 =
-			// econtext.getResourceAsStream("/WEB-INF/reports/rel1.jasper");
-			j.add(JasperFillManager.fillReport(stream1, params, new JRBeanCollectionDataSource(list)));
 
-			params = new HashMap(); // preenche os parâmetros do relatório 2
-			params.put("empresa", empresaServico.findOne(1l));
-			// stream =
-			// econtext.getResourceAsStream("/WEB-INF/reports/rel2.jasper");
-			j.add(JasperFillManager.fillReport(stream2, params,new JRBeanCollectionDataSource(list) ));
+			j.add(JasperFillManager.fillReport(stream1, params, jrRS));
+
+			j.add(JasperFillManager.fillReport(stream2, params, jrRS));
 
 			JRPdfExporter exporter = new JRPdfExporter();
 			HttpServletResponse response = (HttpServletResponse) econtext
