813eb02c04e1b0212eb6d3615ba91e7da11921e7
alvaraesam <alvaraesam@gmail.com>
2014-11-24 21:56:06 +0000

    aadd tabela media turma

--- sge/desenvolvimento/fonte/db/atualizacoes_1.0.0b01.sql
+++ sge/desenvolvimento/fonte/db/atualizacoes_1.0.0b01.sql
@@ -1,2 +1,12 @@
 
 
+DROP TABLE IF EXISTS `acad_media_turma`;
+CREATE TABLE `acad_media_turma` (
+  `id_media_turma` bigint(20) NOT NULL AUTO_INCREMENT,
+  `media1` float NULL,
+  `media2` float NULL,
+  `media3` float NULL,
+  `media4` float NULL,
+  `disciplina` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_media_turma`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
