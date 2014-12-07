a824fa9c8a9fcd72981a00895180fccd2a8b019d
alvaraesam <alvaraesam@gmail.com>
2014-12-07 17:26:08 +0000

    INCLUIR O VALOR DO CURSO COM DESCONTO

--- sge/desenvolvimento/fonte/db/atualizacoes_1.0.0b01.sql
+++ sge/desenvolvimento/fonte/db/atualizacoes_1.0.0b01.sql
@@ -17,5 +17,9 @@ ALTER TABLE `sge`.`acad_boletim` ADD COLUMN `tx_observacao` VARCHAR(255) AFTER `
 -- Add coluna versao em configuracao
 ALTER TABLE `sge`.`acad_configuracao` ADD COLUMN `versao` VARCHAR(10) AFTER `contrato_parte2`;
 
+-- INCLUIR UM CAMPO PARA GUARDA O VALOR COM DESCONTO
+ALTER TABLE `sge`.`acad_curso` 
+ADD COLUMN `valorComDesconto` DOUBLE NULL AFTER `valor`;
+
 update acad_configuracao set versao = '1.0.0b01';
 
